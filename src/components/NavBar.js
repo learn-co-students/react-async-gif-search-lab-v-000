3bab6e25c7dc944ff389a8e20859866e3552169a
Connor Gallopo <connor.gallopo@me.com>
2018-08-21 18:56:52 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function NavBar(props){
+function NavBar(props) {
   const colors = {
     black: 'navbar-inverse',
     white: 'navbar-default'
   }
-  
+
   return (
     <nav className={`navbar ${colors[props.color]}`}>
       <div className='container-fluid'>
         <div className='navbar-header'>
           <a className='navbar-brand'>
-            { props.title }
+            {props.title}
           </a>
         </div>
       </div>
