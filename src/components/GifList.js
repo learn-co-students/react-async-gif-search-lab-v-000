aa39b921f9a56313bd2930043d1abc4dda1cf06e
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-08-23 18:09:00 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -12,6 +12,8 @@ constructor(props){
     return (
     <div>
       {this.props.gifs.map(gif => <img key={gif.url} src={gif.url} alt="gif"/>)}
+
+      <br/> 
     </div>
     )
   }
--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -21,6 +21,7 @@ class GifListContainer extends React.Component {
     fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
       .then(res => res.json())
       .then(({data}) => {
+        console.log(data)
         this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
       })
   }
