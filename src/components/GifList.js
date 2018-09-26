109078a3f869e2c94fc8b53ad5cb917e00483e5a
unenlightened <adept_inept@yahoo.com>
2018-09-26 16:17:44 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -4,8 +4,10 @@ export default class GifList extends Component {
   render() {
     return (
       <div class='float-left'>
-        <ul class='col-sm'>
-          {this.props.gifs.map(gif => <li><img src={gif} /></li>)}
+        <ul>
+          {this.props.gifs.map(gif => (
+          <li><img src={gif} /></li>
+          ))}
         </ul>
       </div>
     )
