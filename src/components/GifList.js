ee926aff275a4521365d0013e5f912d4249f8a98
Su0414 <susanaik.com>
2018-10-28 11:46:02 -0700

    Done.

---
+++ src/components/GifList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+class GifList extends Component {
+    render(){
+        return (
+            <div>
+                {this.props.gifs.map(gif => 
+                <img key={gif.url} src={gif.url} alt="gif"/>)}
+            </div>
+          );
+    }
+}
+
+export default GifList;
