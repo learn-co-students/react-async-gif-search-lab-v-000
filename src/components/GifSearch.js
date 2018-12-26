baa863341ee03339ca3602c539a0331521f1ef54
AubreyNeimeier <neimeier.1@osu.edu>
2018-12-26 15:43:38 -0800

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react'
 
 export default class GifSearch extends Component {
-    constructor(){
-        super()
-        this.state = {
-            query: ''
-        }
-    }
+    state = {
+        query: ""
+      }
+    
+      handleSubmit = event => {
+        event.preventDefault()
+        this.props.fetchGIFs(this.state.query)
+      }
 
     handleChange = (e) =>{
-        e.preventDefault()
         this.setState({
-            query: e.target.value
-        })
-    }
+           query: e.target.value
+       })
+   }
     render (){
         return (
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                debugger
-                this.props.submitFn(this.state.value)}}>
+            <form onSubmit={this.handleSubmit(this.state.value)}>
 
                 Search for Gif's here<input type="text" onChange={this.handleChange} value={this.state.query} />
                 <button> Submit </button>
