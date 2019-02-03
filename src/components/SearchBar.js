35712e7a068d26d3e12d4e97d6937d69022dff00
Micah Shute <micah.shute@gmail.com>
2019-02-02 20:46:50 -0600

    Done.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBar = (props) => {
+
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <input name={props.searchName} value={props.searchValue} onChange={props.handleChange}></input>
+            <button type="submit">Search</button>
+        </form>
+    )
+}
+
+export default SearchBar
