926f72d506d30930e48073d83852b1bc12ee97f2
MiriamPeskowitz <miriam5000@gmail.com>
2019-03-04 12:54:20 -0500

    gif search works

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -17,8 +17,8 @@ class GifListContainer extends Component {
   } 
 
   
-  fetchGifs = (query) => {
-    fetch('http:/api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3')
+  fetchGifs = (query = "dolphins") => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
       .then(response => response.json())
       .then(({data}) => {
         this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url}) ) }) 
