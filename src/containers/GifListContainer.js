18b37011280f9389a45c5de9669843283cc477f4
Emi Ko <emi8emi@gmail.com>
2019-03-15 15:19:26 -0700

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -4,13 +4,27 @@ import GifSearch from '../components/GifSearch'
 
 class GifListContainer extends Component {
 
+  state = {
+   gifs: []
+ }
 
+  componentDidMount() {
+    this.getGifs
+  }
+
+  getGifs = (search) => {
+    console.log(search)
+    fetch('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=dc6zaTOxFJmzC&rating=g')
+    .then(data => data.json())
+    .then(({data}) => {this.setState({ gifs: data.map( gif => ({url: gif.images.original.url}))})})
+  }
 
   render() {
     return(
       <div>
-        
-
+        <GifSearch getGifs={this.getGifs}/>
+        <br />
+        <GifList gifs={this.state.gifs}/>
       </div>
     )
   }
