9892a7a2bcb24fd23b5710dc3f11a4a7b83fd0a4
Heidi Engstrom <hengstrom25@gmail.com>
2019-03-25 21:13:28 +0000

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+ export default class GifSearch extends Component {
+    constructor() {
+        super()
+        this.state = {
+            query: ''
+        }
+    }
+
+     handleChange = event => {
+        this.setState({
+            query: event.target.value
+        })
+    }
+
+     handleSubmit = event => {
+        event.preventDefault();
+        this.props.setQuery(this.state.query)
+    }
+
+
+     render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="searchQuery" onChange={this.handleChange} />
+                <input type="submit" />
+            </form>
+        )
+    }
+} 
