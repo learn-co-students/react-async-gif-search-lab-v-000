ae0af96a9030acc986d702729cd2127ac1d4d525
github username <github email address>
2019-04-05 06:02:03 -0400

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,39 @@
+// create your App component here
+import React, { Component } from 'react'
+import GifListContainer from '../containers/GifListContainer'
+
+
+class GifSearch extends Component {
+
+  state = {
+      query: ""
+  }
+
+  handleChange = event => {
+   this.setState(
+     {query: event.target.value}
+   );
+ }
+
+ handleSubmit = event => {
+   event.preventDefault();
+   this.props.fetchGIFs(this.state.query)
+ }
+
+ render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Gif type:
+          <input type="text" value={this.state.query} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
+
+
+export default GifSearch
