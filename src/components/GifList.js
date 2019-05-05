933c765796eae239f093d42c1a26a805463917ef
SallyZ12 <szasloff@gmail.com>
2019-05-05 14:50:53 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,17 +1,30 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+//
+//
+//     class GifList extends Component {
+//       render() {
+//             const gifPics = this.props.gifs.map((gif, index)=>(
+//               <img key={index} src={gif.url} alt="gif" />
+//             ))
+//               return(
+//             <ul>
+//             <li> {gifPics} </li>
+//             </ul>
+//       )}
+//   }
+// export default GifList
 
+// Functional component for presentation
+import React from 'react';
 
-    class GifList extends Component {
-      render() {
-            const gifPics = this.props.gifs.map((gif, index)=>(
-              <img key={index} src={gif.url} alt="gif" />
-            ))
-              return(
-            <ul>
-            <li> {gifPics} </li>
-            </ul>
-      )}
+  const GifList = props =>{
+    console.log("what are the props?",props)
+    return (
+      <div>
+      {props.gifs.map((gif, index)=>
+          <img key={index} src={gif.url} alt="gif"/>)}
+      </div>
+    )
   }
 
-
-export default GifList
+ export default GifList
