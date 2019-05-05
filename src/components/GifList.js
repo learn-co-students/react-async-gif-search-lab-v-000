e5418df0061fcea911b1859ceed6e9a526848b08
Jayon Kim <jpkim921@gmail.com>
2019-05-05 17:08:53 -0400

    changed GifList from a class component to a presentational component

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,15 +1,29 @@
+// import React from 'react'
+//
+// class GifList extends React.Component {
+//   // this.props.gifs
+//
+//   render() {
+//     return(
+//       <div>
+//         <li><img alt={this.props.gifURL} src={this.props.gifURL} /></li>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default GifList
+
+
+
 import React from 'react'
 
-class GifList extends React.Component {
-  // this.props.gifs
-
-  render() {
-    return(
-      <div>
-        <li><img alt={this.props.gifURL} src={this.props.gifURL} /></li>
-      </div>
-    )
-  }
+const GifList = (props) => {
+  return(
+    <div>
+      <li><img alt={props.gifURL} src={props.gifURL} /></li>
+    </div>
+  )
 }
 
 export default GifList
