e84ce5f7e21e226da0aeafd5bbe988205a1530f8
terryblue99 <terryblue@comcast.net>
2019-06-01 13:19:44 -0500

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,28 +1,35 @@
-import React, {Component} from 'react'
-
+import React, { Component } from 'react'
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
 
 class GifListContainer extends Component {
 
-	state = {
-		threeGifs: []
-	}
+  state = {
+    gifs: []
+  }
 
-	render() {
-		return (
-			GifListContainer
-		)
-	}
+  render() {
+    return(
+      <div>
+        <GifSearch fetchGIFs={this.fetchGIFs} />
+        <GifList gifs={this.state.gifs} />
+      </div>
+    )
+  }
 
-	componentDidMount() {
-		fetch('http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g')
-			.then(res => res.json())
-			.then(data => {
-				this.setState({
-					threeGifs: data.images
-				})
-			})
-	}
+  fetchGIFs = (query) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+      .then(res => res.json())
+      .then(({data}) => {
+        this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+      }).catch(function(error) {
+      			alert('Not Found! ' + error)
+      		})
+  }
 
+  componentDidMount() {
+    this.fetchGIFs()
+  }
 }
 
 export default GifListContainer
