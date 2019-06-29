f2a3a0dcaf2b36765804f79107440e7abaeacb32
NotoriousCottonball <gpianka1@gmail.com>
2019-06-29 01:40:57 -0700

    Alternative Fetch

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -24,6 +24,16 @@ export default class GifListContainer extends Component {
       })
   }
 
+  fetchGifs = (query = "dolphins") => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data.data)
+        this.setState({ gifs: data.data.map( gif => ({ url: gif.images.original.url }) ) })
+      })
+  }
+
+
   componentDidMount() {
      this.fetchGIFS()
   }
