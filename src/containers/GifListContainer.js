5123ca2a74e9f9394bdec80943a0554bbe978a26
catherinism <catcreates13@gmail.com>
2019-07-20 13:18:46 -0400

    Done.

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import GifSearch from '../components/GifSearch'
+import GifList from '../components/GifList'
+
+export default class GifListContainer extends Component {
+
+  state = {
+    gifs: [],
+  }
+  
+  componentDidMount(){
+    this.fetchGIFs()
+  }
+
+  fetchGIFs = (query) => {
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`).then(res => res.json()).then(({data}) => {
+      this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+    })
+  }
+
+  
+  render(){
+    return <div><GifSearch fetchGIFs={this.fetchGIFs} /><GifList gifs={this.state.gifs}/></div>
+  }
+
+}
