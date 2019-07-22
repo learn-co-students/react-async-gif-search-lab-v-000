13043b4c49af086ff2a8f24e2ada71d8db936606
Rose Brennan <rose@grubhub.com>
2019-07-21 21:06:30 -0600

    Done.

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
+import NavBar from '../components/NavBar'
+
+class GifListContainer extends Component {
+
+  state = {
+    gifs: []
+  }
+
+  componentDidMount() {
+    fetch(`http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g/.json?limit=3`)
+    .then(response => response.json())
+    .then(gifs => this.setState({ gifs }))
+  }
+
+  render() {
+
+
+    return(
+      <div className="gif list container">
+        < GifList listOfGifs={this.componentDidMount}/>
+        <GifSearch />
+      </div>
+    )
+
+  }
+
+}
+
+export default GifListContainer
