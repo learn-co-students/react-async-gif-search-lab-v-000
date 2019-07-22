8c6a7c402160534a2869005da7a9201ee9d97e63
Rose Brennan <rose@grubhub.com>
2019-07-22 11:00:08 -0600

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
-import NavBar from '../components/NavBar'
 
 class GifListContainer extends Component {
 
@@ -10,10 +9,10 @@ class GifListContainer extends Component {
     gifs: []
   }
 
-  fetchGifs = () => {
-    fetch(`http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g/.json?limit=3`)
-    .then(response => response.json())
-    .then(gifs => this.setState({ gifs }))
+  fetchGifs = (query) => {
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+      .then(response => response.json())
+      .then(gifs => this.setState({ gifs }))
   }
 
   render() {
