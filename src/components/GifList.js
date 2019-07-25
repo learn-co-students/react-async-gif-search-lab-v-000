5d46b27674c0651c5be779257455fb04b753f1f0
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-25 14:31:25 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GifList = props => {
   return (
     <div>
-      {props.gifs.map(gif => <img key={gif.url} src={gif.url} />)}
+      {props.gifs.map(gif => <img key={gif.url} src={gif.url} alt={''} />)}
     </div>
   )
 }
--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const GifSearch = () => {
-  return (
+class GifSearch extends Component {
 
-  )
+  state = {
+    value: ''
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    this.props.fetchGifs(this.state.value);
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div style={{float:'right'}}>
+        <p>Enter a Search Term:</p>
+        <form>
+          <input
+            type='text'
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </form>
+        <button onClick={e => this.handleClick(e)}>Search</button>
+      </div>
+    )
+  }
 }
 
 export default GifSearch;
