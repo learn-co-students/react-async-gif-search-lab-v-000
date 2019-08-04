fa80dc38f9a4b79f4dd6b33544c2c6501d184696
nmarini <nmarini44@gmail.com>
2019-08-04 15:58:30 -0700

    Done.

---
+++ src/components/GifList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class GifList extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.topGifs.map((gif) => (
+                        <li key={gif}><img src={gif} alt="gif"/></li>
+                    ))}
+                </ul>
+            </div>
+            
+        )
+    }
+    
+}
+
+export default GifList;
---
+++ src/components/GifSearch.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class GifSearch extends Component {
+
+    state = {
+        query : ''
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.fetchApi(this.state.query)
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.query} onChange={event => this.setState({query: event.target.value})}/>
+            </form>
+        )
+    }
+}
+
+export default GifSearch;
