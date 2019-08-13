cd9db600fd68f9cc62640070294008d50c9d5bfd
Kazumi Karbowski <kazkazinus@gmail.com>
2019-08-12 22:23:32 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,12 @@ import React from 'react'
 import NavBar from './NavBar'
 import GifListContainer from '../containers/GifListContainer'
 
-// import GifList from './GifList'
-// import GifSearch from './GifSearch'
-
 const App = () => {
   
   return (
     <div>
         < NavBar color='black' title="Giphy Search" />
         <GifListContainer />
-        
     </div>
   )
 }
--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,5 +1,3 @@
-//<GifList /> is a presentational component. 
-//It receives data from its props and renders html given the input data. It can render a top level <ul> with each gif as an <li>
 import React from 'react'
 
 const GifList = props => {
@@ -11,16 +9,3 @@ const GifList = props => {
 }
 
 export default GifList;
-
-// import React from 'react';
-// import GifListContainer from '../containers/GifListContainer'
-
-
-// const GifList = props => 
-//   <ul>
-//   {props.gifs.map(gif => (
-//     console.log(gif)
-//     // <li>{c.body}—{c.author}</li>
-//   ))}
-//   </ul>
-// export default GifList;
