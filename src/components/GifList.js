9ab2413858025d5b96dab7157b1f5f1af0efd14f
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-10-30 19:40:38 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,18 +3,13 @@ import React, { Component } from "react";
 class GifList extends Component {
   constructor(props) {
     super(props);
-    this.data = {
-      data: this.props.gif
-    }
   }
   
   render() {
     return (
-      <React.Fragment>
       <ul>
-      <li>{this.props.data}</li>
+        {this.props.data.map((u)=> (<li><img src={u.url}/></li>))}
       </ul>
-      </React.Fragment>
     )
   }
 }
