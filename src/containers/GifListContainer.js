56e62c9090d49b753ecbdeb3ec3691248f7e9a01
github username <github email address>
2020-08-25 19:37:00 -0700

    Done.

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import App from '.src/components/App'
+
+class GifListContainer extends App {
+
+  state = {
+    gifs = []
+  }
+
+
+  render(){
+    return(
+      <div>hi </div>
+    )
+  }
+
+
+
+}
+
+export default GifListContainer
---
+++ src/containers/new.js
@@ -0,0 +1,7 @@
+
+
+<form onSubmit={this.handleSubmit}>
+  <input type="text" name="username" value={this.state.username} />
+  <input type="text" name="password" value={this.state.password} />
+
+</form>
