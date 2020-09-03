8eb5f3819cca47fdc037fdbd946f5123cb26c4b9
Joshua Needham <joshuabneedham@gmail.com>
2020-09-03 09:56:23 -0500

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function GifList(props) {
-    let gifArray = props.gifs.map((gifObj) => <li><img src={gifObj.images.original.url} /></li>)
-    console.log("gif array: ", gifArray)
-    return (
-        <ul>
-            {gifArray}
-        </ul>
-    )
+	let gifArray = props.gifs.map((gifObj) => (
+		<li key={gifObj.id}>
+			<img src={gifObj.images.original.url} />
+		</li>
+	));
+	console.log('gif array: ', gifArray);
+	return <ul>{gifArray}</ul>;
 }
 
-export default GifList
+export default GifList;
