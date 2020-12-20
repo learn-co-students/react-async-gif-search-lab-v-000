a176d3f1e772dc6bbbc4fb082ce4637187c145bf
RonSala <ron.sala@gmail.com>
2020-12-20 10:03:30 -0500

    test .env

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+const MY_KEY = process.env.REACT_APP_API_KEY;
+    console.log('MY_KEY:', MY_KEY);
+
 class GifListContainer extends React.Component {
   state = { gifs: [] };
 
   componentDidMount(query) {
+    // const MY_KEY = process.env.REACT_APP_API_KEY;
+    // console.log('MY_KEY:', MY_KEY);
     // const q = query
-    fetch(`https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=YOUR API KEY&rating=g`)
+    fetch(`https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=${MY_KEY}&rating=g`)
   }  
 }
 
